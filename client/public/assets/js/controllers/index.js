5fcef4d49a89d47fc41f070d0c49743c4b632dce
Pankamol Srikaew <aim.ske09@gmail.com>
2017-10-14 00:35:17 +0700

    send http request to server

--- client/public/assets/js/controllers/index.js
+++ client/public/assets/js/controllers/index.js
@@ -1,6 +1,13 @@
 angular.module('app')
 .controller('IndexCtrl',['$scope', '$http','$window',
 	function($scope, $http, $window){
+		$scope.search = function(){
+			$http.get('http://localhost:3000/'+$scope.githubId)
+			.then(function(res){
+				$scope.followers = res.data
+			})
+		}
+
 
 	}
 ])
